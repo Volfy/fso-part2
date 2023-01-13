8213aa83440b84b7a755e0fbb80558b7eeb13ed3
sasha v <volfym79@gmail.com>
2023-01-13 14:27:21 +0100

    exercises 2.6,2.7

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,24 +1,55 @@
 import { useState } from 'react'
 
+const Number = ({name}) => <li>{name}</li>
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+
+    // some validation
+    if(!newName) {
+      alert('No name given')
+      return
+    }
+
+    // Not checking for case variations
+    const isUnique = !persons.filter(p => p.name === newName).length 
+
+    if (isUnique){
+      const Name = {
+        name: newName,
+      }
+      setPersons(persons.concat(Name))
+      setNewName('')
+    } else {
+      alert(`${newName} is already in the phonebook`)
+    }
+  }
+
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {/* CODE MISSING */}
+      <ul>
+        {persons.map(x => <Number key={x.name} name={x.name}/>)}
+      </ul>
     </div>
   )
 }
