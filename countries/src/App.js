7f4775e00a412f1c69e5095901a4a36a82123888
sasha v <volfym79@gmail.com>
2023-01-15 20:11:43 +0100

    2.12* done

--- countries/src/App.js
+++ countries/src/App.js
@@ -7,6 +7,26 @@ const Filter = ({nameFilter, handleChange}) =>
     <input value={nameFilter} onChange={handleChange}/>
 </form>
 
+const Country = ({single, name, area, capital, flag, languages}) => {
+  if (single) {
+    return (
+      <div>
+        <h2>{name}</h2>
+        <div>capital {capital}</div>
+        <div>area {area}</div>
+        <div>
+          <h3>languages:</h3>
+          <ul>
+            {Object.values(languages).map(x => <li key={x}>{x}</li>)}
+            
+          </ul> 
+        </div>
+        <img src={flag} alt="flag" />
+      </div>
+    ) 
+  } else return (<div>{name}</div>)
+}
+
 
 function App() {
   const [countries, setCountries] = useState([])
@@ -22,7 +42,8 @@ function App() {
             area: c.area,
             capital: c.capital ? c.capital[0] : undefined,
             languages: c.languages,
-            flag: c.flags.png
+            flag: c.flags.png,
+            key: c.name.common
           }
         })
         setCountries(x)
@@ -34,11 +55,25 @@ function App() {
   }
 
   // console.log(countries)
- 
+/*persons
+  .filter(x => x.name.toLowerCase().includes(nameFilter.toLowerCase()))
+  .map(x => <Number key={x.name} name={x.name} number={x.number}/>)
+*/
+  const matches = countries.filter(x => x.name.toLowerCase().includes(nameFilter.toLowerCase()))
 
   return (
     <div>
       <Filter nameFilter={nameFilter} handleChange={handleChange}/>
+      {matches.length === 1
+            ? <Country single={true} key={matches[0].name}
+                name={matches[0].name} 
+                capital={matches[0].capital}
+                area={matches[0].area}
+                languages={matches[0].languages}
+                flag={matches[0].flag}/>
+            : matches.length < 10 
+                    ? matches.map(x => <Country key={x.name} name={x.name} />)
+                    : <div>Too Many Matches</div>}
     </div>
   );
 }
