ecd01aca71dc6d1e7be74c24beebe9fbb0d3de57
sasha v <volfym79@gmail.com>
2023-01-16 10:48:15 +0100

    2.13* Sortof

--- countries/src/App.js
+++ countries/src/App.js
@@ -7,24 +7,45 @@ const Filter = ({nameFilter, handleChange}) =>
     <input value={nameFilter} onChange={handleChange}/>
 </form>
 
-const Country = ({single, name, area, capital, flag, languages}) => {
-  if (single) {
-    return (
-      <div>
-        <h2>{name}</h2>
-        <div>capital {capital}</div>
-        <div>area {area}</div>
-        <div>
-          <h3>languages:</h3>
-          <ul>
-            {Object.values(languages).map(x => <li key={x}>{x}</li>)}
-            
-          </ul> 
-        </div>
-        <img src={flag} alt="flag" />
-      </div>
-    ) 
-  } else return (<div>{name}</div>)
+const FullCountry = ({name, area, capital, flag, languages}) => (
+  <div>
+    <h2>{name}</h2>
+    <div>capital {capital}</div>
+    <div>area {area}</div>
+    <div>
+      <h3>languages:</h3>
+      <ul>
+        {Object.values(languages).map(x => <li key={x}>{x}</li>)}
+        
+      </ul> 
+    </div>
+    <img src={flag} alt="flag" />
+  </div>
+)
+
+const CollapseCountry = ({name, handleShow}) => (
+  <div>
+    {name} &nbsp;
+    <button onClick={handleShow} country={name}>show</button>
+  </div>
+)
+
+const Countries = ({matches, handleShow}) => {
+  
+  if (matches.length === 1) {
+    return <FullCountry 
+      key={matches[0].name}
+      name={matches[0].name} 
+      capital={matches[0].capital}
+      area={matches[0].area}
+      languages={matches[0].languages}
+      flag={matches[0].flag}
+    />
+  } else if (matches.length === 0) {return <div>No Matches</div>}
+  else if (matches.length > 10) {return <div>Too Many Matches</div>}
+  else {
+    return matches.map(x => <CollapseCountry key={x.name} name={x.name} handleShow={handleShow}/>)
+  }
 }
 
 
@@ -50,30 +71,20 @@ function App() {
       })
   }, [])
 
+  let matches = countries.filter(x => x.name.toLowerCase().includes(nameFilter.toLowerCase()))
+
   const handleChange = (event) => {
     setNameFilter(event.target.value)
   }
 
-  // console.log(countries)
-/*persons
-  .filter(x => x.name.toLowerCase().includes(nameFilter.toLowerCase()))
-  .map(x => <Number key={x.name} name={x.name} number={x.number}/>)
-*/
-  const matches = countries.filter(x => x.name.toLowerCase().includes(nameFilter.toLowerCase()))
+  const handleShow = (event) => {
+    setNameFilter(event.target.getAttribute('country'))
+  }
 
   return (
     <div>
       <Filter nameFilter={nameFilter} handleChange={handleChange}/>
-      {matches.length === 1
-            ? <Country single={true} key={matches[0].name}
-                name={matches[0].name} 
-                capital={matches[0].capital}
-                area={matches[0].area}
-                languages={matches[0].languages}
-                flag={matches[0].flag}/>
-            : matches.length < 10 
-                    ? matches.map(x => <Country key={x.name} name={x.name} />)
-                    : <div>Too Many Matches</div>}
+      <Countries matches={matches} handleShow={handleShow}/> 
     </div>
   );
 }
